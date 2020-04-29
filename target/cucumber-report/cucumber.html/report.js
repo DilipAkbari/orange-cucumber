8fb703d1657540a87eae7f871d3e1ea8c10a9f09
DilipAkbari <akbaridb@gmail.com>
2020-04-29 18:22:22 +0100

    org1

--- target/cucumber-report/cucumber.html/report.js
+++ target/cucumber-report/cucumber.html/report.js
@@ -13,7 +13,7 @@ formatter.feature({
   ]
 });
 formatter.before({
-  "duration": 17272909600,
+  "duration": 16272776100,
   "status": "passed"
 });
 formatter.scenario({
@@ -69,7 +69,7 @@ formatter.match({
   "location": "MyStepdefs.iAmOnHomePage()"
 });
 formatter.result({
-  "duration": 850531500,
+  "duration": 425303700,
   "status": "passed"
 });
 formatter.match({
@@ -82,7 +82,7 @@ formatter.match({
   "location": "MyStepdefs.iEnterUsername(String)"
 });
 formatter.result({
-  "duration": 1026205400,
+  "duration": 306821700,
   "status": "passed"
 });
 formatter.match({
@@ -95,28 +95,28 @@ formatter.match({
   "location": "MyStepdefs.iEnterPassword(String)"
 });
 formatter.result({
-  "duration": 169330500,
+  "duration": 229579300,
   "status": "passed"
 });
 formatter.match({
   "location": "MyStepdefs.iClickOnLoginLink()"
 });
 formatter.result({
-  "duration": 1600596600,
+  "duration": 1373168400,
   "status": "passed"
 });
 formatter.match({
   "location": "MyStepdefs.iClickOnPinButton()"
 });
 formatter.result({
-  "duration": 51100,
+  "duration": 37500,
   "status": "passed"
 });
 formatter.match({
   "location": "MyStepdefs.iClickOnAddEmployeeButon()"
 });
 formatter.result({
-  "duration": 53100,
+  "duration": 34500,
   "status": "passed"
 });
 formatter.match({
@@ -129,12 +129,12 @@ formatter.match({
   "location": "MyStepdefs.iShouldSeeEmployeeId(String)"
 });
 formatter.result({
-  "duration": 10460266500,
-  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//h1[contains(text(),\u0027Add Employee\u0027)]\"}\n  (Session info: chrome\u003d81.0.4044.122)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-59P8T20\u0027, ip: \u0027192.168.0.6\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_121\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 81.0.4044.122, chrome: {chromedriverVersion: 80.0.3987.106 (f68069574609..., userDataDir: C:\\Users\\HP\\AppData\\Local\\T...}, goog:chromeOptions: {debuggerAddress: localhost:57928}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: e2cbb08be4685733f44f2962ae798884\n*** Element info: {Using\u003dxpath, value\u003d//h1[contains(text(),\u0027Add Employee\u0027)]}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:69)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:38)\r\n\tat com.sun.proxy.$Proxy18.getText(Unknown Source)\r\n\tat com.orangehrmlive.demo.utility.Utility.getTextFromElement(Utility.java:52)\r\n\tat com.orangehrmlive.demo.pages.AccountPage.verifyEmployeeIdext(AccountPage.java:69)\r\n\tat com.orangehrmlive.demo.cucumber.stepdefs.MyStepdefs.iShouldSeeEmployeeId(MyStepdefs.java:61)\r\n\tat ✽.Then I should see employee id \"Add Employee\"(src/test/java/com/orangehrmlive/demo/resources/featurefile/account.feature:12)\r\n",
+  "duration": 10864887700,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//h1[contains(text(),\u0027Add Employee\u0027)]\"}\n  (Session info: chrome\u003d81.0.4044.122)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-KKCD7HH\u0027, ip: \u0027192.168.0.10\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_121\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 81.0.4044.122, chrome: {chromedriverVersion: 80.0.3987.106 (f68069574609..., userDataDir: C:\\Users\\akbar\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:57380}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: ab73278853c77ec6241c4d5d173f3f91\n*** Element info: {Using\u003dxpath, value\u003d//h1[contains(text(),\u0027Add Employee\u0027)]}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:69)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:38)\r\n\tat com.sun.proxy.$Proxy18.getText(Unknown Source)\r\n\tat com.orangehrmlive.demo.utility.Utility.getTextFromElement(Utility.java:52)\r\n\tat com.orangehrmlive.demo.pages.AccountPage.verifyEmployeeIdext(AccountPage.java:69)\r\n\tat com.orangehrmlive.demo.cucumber.stepdefs.MyStepdefs.iShouldSeeEmployeeId(MyStepdefs.java:61)\r\n\tat ✽.Then I should see employee id \"Add Employee\"(src/test/java/com/orangehrmlive/demo/resources/featurefile/account.feature:12)\r\n",
   "status": "failed"
 });
 formatter.after({
-  "duration": 2142101200,
+  "duration": 4338555100,
   "status": "passed"
 });
 formatter.uri("src/test/java/com/orangehrmlive/demo/resources/featurefile/login.feature");
@@ -152,7 +152,7 @@ formatter.feature({
   ]
 });
 formatter.before({
-  "duration": 15238040500,
+  "duration": 13134853800,
   "status": "passed"
 });
 formatter.scenario({
@@ -198,7 +198,7 @@ formatter.match({
   "location": "MyStepdefs.iAmOnHomePage()"
 });
 formatter.result({
-  "duration": 68300,
+  "duration": 45800,
   "status": "passed"
 });
 formatter.match({
@@ -211,7 +211,7 @@ formatter.match({
   "location": "MyStepdefs.iEnterUsername(String)"
 });
 formatter.result({
-  "duration": 151686200,
+  "duration": 740956000,
   "status": "passed"
 });
 formatter.match({
@@ -224,29 +224,29 @@ formatter.match({
   "location": "MyStepdefs.iEnterPassword(String)"
 });
 formatter.result({
-  "duration": 142799100,
+  "duration": 312743500,
   "status": "passed"
 });
 formatter.match({
   "location": "MyStepdefs.iClickOnLoginLink()"
 });
 formatter.result({
-  "duration": 1659777200,
+  "duration": 1677408200,
   "status": "passed"
 });
 formatter.match({
   "location": "MyStepdefs.iShouldLoginSuccessfully()"
 });
 formatter.result({
-  "duration": 1500427900,
+  "duration": 1374899700,
   "status": "passed"
 });
 formatter.after({
-  "duration": 976693200,
+  "duration": 1752732000,
   "status": "passed"
 });
 formatter.before({
-  "duration": 13038144300,
+  "duration": 12408177700,
   "status": "passed"
 });
 formatter.scenario({
@@ -297,7 +297,7 @@ formatter.match({
   "location": "MyStepdefs.iAmOnHomePage()"
 });
 formatter.result({
-  "duration": 638600,
+  "duration": 47100,
   "status": "passed"
 });
 formatter.match({
@@ -310,7 +310,7 @@ formatter.match({
   "location": "MyStepdefs.iEnterUsername(String)"
 });
 formatter.result({
-  "duration": 149896100,
+  "duration": 436211500,
   "status": "passed"
 });
 formatter.match({
@@ -323,21 +323,21 @@ formatter.match({
   "location": "MyStepdefs.iEnterPassword(String)"
 });
 formatter.result({
-  "duration": 149558000,
+  "duration": 405996800,
   "status": "passed"
 });
 formatter.match({
   "location": "MyStepdefs.iClickOnLoginLink()"
 });
 formatter.result({
-  "duration": 1484000400,
+  "duration": 1804374500,
   "status": "passed"
 });
 formatter.match({
   "location": "MyStepdefs.iClickOnWelcomeAdminLinkAndLogoutLink()"
 });
 formatter.result({
-  "duration": 12356986600,
+  "duration": 12702638100,
   "status": "passed"
 });
 formatter.match({
@@ -350,11 +350,11 @@ formatter.match({
   "location": "MyStepdefs.iShouldSeeLoginPannel(String)"
 });
 formatter.result({
-  "duration": 123740200,
+  "duration": 93301600,
   "status": "passed"
 });
 formatter.after({
-  "duration": 860363500,
+  "duration": 1707045500,
   "status": "passed"
 });
 });
